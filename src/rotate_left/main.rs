be733581c6f7e01361ed2fe402d5cdc895e8b3ea
szabo <aleqvids@gmail.com>
2023-01-23 12:49:34 +0300

    updates

--- src/rotate_left/main.rs
+++ src/rotate_left/main.rs
@@ -1,30 +1,73 @@
 use std::io::{self, BufRead};
 
 /*
- * Complete the 'rotateLeft' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER d
- *  2. INTEGER_ARRAY arr
- */
+* Complete the 'rotateLeft' function below.
+*
+* The function is expected to return an INTEGER_ARRAY.
+* The function accepts following parameters:
+*  1. INTEGER d
+*  2. INTEGER_ARRAY arr
+*/
 
-fn rotateLeft(d: i32, arr: &[i32]) {
-    println!("{}", d);
-    println!("{}", arr[0]);
+/* 
+sample input:
+5 4
+1 2 3 4 5
 
+sample output:
+5 1 2 3 4
+*/
 
+fn rotate_left(d: i32, arr: &[i32]) -> Vec<i32> {
+    let mut i = d;
 
+    let mut rotate_arr: Vec<i32> = Vec::with_capacity(d as usize);
+
+    /* 
+    d: 2
+    arr: 1 2 3 4 5
+
+    rotate_arr: 3 4 5
+    */
+
+    while i < arr.len() as i32 {
+        let item = arr[i as usize];
+        rotate_arr.push(item);
+
+        i += 1;
+    }
+
+    /* 
+    d: 2
+    i: 0
+    arr: 1 2 3 4 5
+         x x
+    
+    rotate_arr: 3 4 5 1 2
+    */
+
+    i = 0;
+    while i < d {
+        let item = arr[i as usize];
+        rotate_arr.push(item);
+
+        i += 1;
+    }
+
+    return rotate_arr;
 }
 
 fn main() {
     let stdin = io::stdin();
     let mut stdin_iterator = stdin.lock().lines();
 
+    // size of array
     let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
 
-    println!("n: {}", n);
+    // number of rotations
+    let d = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
 
+    // initializing input array
     let mut arr: Vec<i32> = Vec::with_capacity(n as usize);
 
     for _ in 0..n {
@@ -32,13 +75,10 @@ fn main() {
         arr.push(array_item);
     }
 
-    println!("printing array");
+    let result = rotate_left(d, &arr);
 
-    for i in &arr {
+    for i in result {
         println!("{}", i);
     }
-
-    println!("passing arr to func");
-
-    rotateLeft(n, &arr);
 }
+
